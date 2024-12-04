c5b9e9fe095ed3bf7c99d5c95b1fb47d6dbc29d0
Nish <nlankala@horizon.csueastbay.edu>
2024-12-04 14:04:09 -0800

    Feat : persist state to local storage

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,77 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+function loadFromLocalStorage() {
+  let cart = localStorage.getItem("cart");
 
-const products = [
-  {
-    id : 1,
-    name: "Airpods Wireless Bluetooth Headphones",
-    image: "/images/airpods.jpg",
-    price: 89.99,
-    quantity : 2,
-    quantityInStock: 10,
-  },
-  {
-    id : 2,
-    name: "iPhone 11 Pro 256GB Memory",
-    image: "/images/phone.jpg",
-    price: 599.99,
-    quantity : 3,
-    quantityInStock: 7,
-  },
-  {
-    id : 3,
-    name: "Cannon EOS 80D DSLR Camera",
-    image: "/images/camera.jpg",
-    price: 929.99,
-    quantity : 4,
-    quantityInStock: 5,
-  },
-  {
-    id:4,
-    name: "Sony Playstation 4 Pro White Version",
-    image: "/images/playstation.jpg",
-    price: 399.99,
-    quantity : 5,
-    quantityInStock: 11,
-  },
-  {
-    id : 5,
-    name: "Logitech G-Series Gaming Mouse",
-    image: "/images/mouse.jpg",
-    price: 49.99,
-    quantity:1,
-    quantityInStock: 7,
-  },
-];
+  if (cart) {
+    cart = JSON.parse(cart);
+  }
 
-
-
-const initialCartSlice = {
-  cart : []
-};
+  return cart;
+}
 
 const cartSlice = createSlice({
   name: "cart",
-  initialState: initialCartSlice,
+  initialState: loadFromLocalStorage() || { cart: [] },
   reducers: {
     addProduct(state, action) {
       state.cart.push({
-       
-        id : action.payload.id,
-        name : action.payload.name,
-        image : action.payload.image,
-        price : action.payload.price,
-        quantity : 1,
-        quantityInStock : action.payload.quantityInStock
-       
-
+        id: action.payload.id,
+        name: action.payload.name,
+        image: action.payload.image,
+        price: action.payload.price,
+        quantity: 1,
+        quantityInStock: action.payload.quantityInStock,
       });
     },
 
     increaseQuantity(state, action) {
-
-
-      const product = state.cart.find((product) => product.id === action.payload.id);
+      const product = state.cart.find(
+        (product) => product.id === action.payload.id
+      );
 
       if (product.quantityInStock > product.quantity) {
         product.quantity++;
@@ -79,18 +36,19 @@ const cartSlice = createSlice({
     },
 
     decreaseQuantity(state, action) {
-      const product = state.cart.find((product) => product.id === action.payload.id);
+      const product = state.cart.find(
+        (product) => product.id === action.payload.id
+      );
 
       if (product.quantity > 1) product.quantity--;
       else {
-        state.cart = state.cart.filter((product) => product.id !== action.payload.id);
+        state.cart = state.cart.filter(
+          (product) => product.id !== action.payload.id
+        );
       }
     },
 
     removeProduct(state, action) {
-
-      
-
       state.cart = state.cart.filter((product) => {
         return product.id !== action.payload.id;
       });
@@ -98,9 +56,7 @@ const cartSlice = createSlice({
   },
 });
 
+export const { addProduct, removeProduct, increaseQuantity, decreaseQuantity } =
+  cartSlice.actions;
 
-export const {addProduct,removeProduct,increaseQuantity,decreaseQuantity} = cartSlice.actions
-
-export default cartSlice.reducer
-
-
+export default cartSlice.reducer;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,10 +2,26 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import cartReducer from "./slices/cartSlice";
 
+const persistStateToLocalStorage = (storeApi) => (next) => (action) => {
+  //calls the reducer. let the reducer perform the action first
+  const result = next(action);
+
+  const state = storeApi.getState();
+
+  if (action.type.startsWith("cart")) {
+    console.log("saving to localstorage")
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }
+
+  return result;
+};
+
 const store = configureStore({
   reducer: {
     cart: cartReducer,
   },
+  middleware: (getDefaultMiddlware) =>
+    getDefaultMiddlware().concat(persistStateToLocalStorage),
 });
 
-export default store
+export default store;
